3fe1899a12a1a88e861b00a3ba3ea66e6ed545e4
AntonD3 <antonv.dyadyuk@gmail.com>
2023-04-28 23:09:06 +0100

    zkSyncTestnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,8 @@ import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-etherscan'
 import 'hardhat-contract-sizer'
 import 'solidity-coverage'
+import '@matterlabs/hardhat-zksync-solc'
+import '@matterlabs/hardhat-zksync-verify'
 
 import { HardhatUserConfig } from 'hardhat/config'
 import { SolcUserConfig } from 'hardhat/types'
@@ -56,9 +58,24 @@ const config: HardhatUserConfig = {
     kovan: {
       url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
     },
+    zkSyncTestnet: {
+      url: "https://testnet.era.zksync.dev",
+      ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      zksync: true,
+      verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
+    },
   },
   solidity: {
     compilers: [DEFAULT_COMPILER_SETTINGS],
+  },
+  zksolc: {
+    version: "1.3.10",
+    compilerSource: "binary",
+    settings: {
+      metadata: {
+        bytecodeHash: 'none',
+      },
+    },
   },
   contractSizer: {
     alphaSort: false,
